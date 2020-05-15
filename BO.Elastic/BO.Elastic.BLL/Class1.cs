1ae6e409edbb8bec0b652a2f4b1aab7e3ddd7403
Oskar Szymański [OPONEO.PL S.A.] <oszymanski@opony.local>
2020-05-15 14:22:56 +0200

    EntityFramework

---
+++ BO.Elastic/BO.Elastic.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BO.Elastic.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ BO.Elastic/BO.Elastic.BLL/Model/ElasticContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BO.Elastic.BLL.Model
+{
+    public partial class ElasticContext : DbContext
+    {
+        public ElasticContext()
+        {
+        }
+
+        public ElasticContext(DbContextOptions<ElasticContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=10.10.1.214;Database=Elastic; User Id=Panel; Password=qwerty;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
