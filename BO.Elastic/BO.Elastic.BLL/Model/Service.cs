f91e59e58e2a9b7c162d1c2a4a89b497b3b6b4a0
capthapl <dawidkacprzak@icloud.com>
2020-05-16 22:25:40 +0200

    Updated db context

---
+++ BO.Elastic/BO.Elastic.BLL/Model/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BO.Elastic.BLL.Model
+{
+    public partial class Service
+    {
+        public Service()
+        {
+            ClusterNodeCluster = new HashSet<ClusterNode>();
+            ClusterNodeNode = new HashSet<ClusterNode>();
+        }
+
+        public int Id { get; set; }
+        public int ServiceType { get; set; }
+        public string Ip { get; set; }
+        public string Port { get; set; }
+
+        public virtual ServiceType ServiceTypeNavigation { get; set; }
+        public virtual ICollection<ClusterNode> ClusterNodeCluster { get; set; }
+        public virtual ICollection<ClusterNode> ClusterNodeNode { get; set; }
+    }
+}
