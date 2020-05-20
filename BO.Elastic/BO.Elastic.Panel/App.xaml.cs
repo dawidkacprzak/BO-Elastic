1391bece5d58d7d0b12de936bc84eb153e083f24
capthapl <dawidkacprzak@icloud.com>
2020-05-20 22:27:16 +0200

    Added update check process and prism implementation (without app update)

--- BO.Elastic/BO.Elastic.Panel/App.xaml.cs
+++ BO.Elastic/BO.Elastic.Panel/App.xaml.cs
@@ -1,17 +1,46 @@
-﻿using System;
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace BO.Elastic.Panel
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+        protected override Window CreateShell()
+        {
+            try
+            {
+                Views.LoadingWindow w = Container.Resolve<Views.LoadingWindow>();
+                return w;
+            }catch(Exception ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Nieobsłużony wyjątek: "+e.Exception.Message);
+            e.Handled = true;
+        }
     }
 }
