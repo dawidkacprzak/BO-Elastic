1c9edbc8ddf7eaff14c1705cc80fdecb78ed6cc4
capthapl <dawidkacprzak@icloud.com>
2020-05-21 19:21:44 +0200

    Created simple updater

--- BO.Elastic/BO.Elastic.Panel/ViewModels/LoadingWindowViewModel.cs
+++ BO.Elastic/BO.Elastic.Panel/ViewModels/LoadingWindowViewModel.cs
@@ -3,9 +3,13 @@ using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
@@ -47,9 +51,19 @@ namespace BO.Elastic.Panel.ViewModels
 
         public void RunApplication(Action<string> changeStatusEvent)
         {
+            DeleteOldFiles();
             if (IsUpdateAvailable())
             {
-                changeStatusEvent("Aktualizacja dostępna - trwa pobieranie.");
+                try
+                {
+                    changeStatusEvent("Aktualizacja dostępna - trwa pobieranie.");
+                    UpdateApplication();
+                    ForceRestartApplication();
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("Błąd podczasz aktualizacji aplikacji " + ex.Message);
+                }
+
             }
             else
             {
@@ -60,6 +74,115 @@ namespace BO.Elastic.Panel.ViewModels
         public void CloseApplication()
         {
             Application.Current.Shutdown();
+        }
+
+        private void ForceRestartApplication()
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = Path.Combine(Directory.GetCurrentDirectory(), Process.GetCurrentProcess().ProcessName);
+            if (!p.Start()) MessageBox.Show("Błąd podczas restartu aktualizacji");
+            CloseApplication();
+        }
+
+        private void DeleteOldFiles()
+        {
+            string[] filesInAppDirectory = Directory.GetFiles(Directory.GetCurrentDirectory());
+            string[] directoriesInAppDirectory = Directory.GetDirectories(Directory.GetCurrentDirectory());
+
+            foreach (string file in filesInAppDirectory.Where(x => x.EndsWith(".old")).ToArray<string>())
+            {
+                try
+                {
+                    if (File.Exists(file))
+                    {
+                        File.Delete(file);
+                    }
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("Błąd podczas usuwania plików tymczasowych starej aktualizacji " + ex.Message);
+                }
+            }
+
+            foreach (string file in directoriesInAppDirectory.Where(x => x.EndsWith(".old")).ToArray<string>())
+            {
+                try
+                {
+                    if (Directory.Exists(file))
+                    {
+                        Directory.Delete(file,true);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd podczas usuwania folderów tymczasowych starej aktualizacji " + ex.Message);
+                }
+            }
+        }
+
+        private void UpdateApplication()
+        {
+            string downloadedFileName = Guid.NewGuid().ToString()+".zip";
+            string downloadedFileFullPath = Path.Join(Path.GetTempPath(), downloadedFileName);
+            string[] filesInAppDirectory = Directory.GetFiles(Directory.GetCurrentDirectory());
+            string[] directoriesInAppDirectory = Directory.GetDirectories(Directory.GetCurrentDirectory());
+
+            string[] filesInDownloadedBuild;
+            string[] directoriesInDownloadedBuild;
+
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadFile("http://213.32.122.228/Bo.Elastic.Panel/build.zip", downloadedFileFullPath);
+            }
+
+            string tempUpdateDirectory = Path.Join(Path.GetTempPath(),Guid.NewGuid().ToString());
+
+            if (!Directory.Exists(tempUpdateDirectory))
+            {
+                Directory.CreateDirectory(tempUpdateDirectory);
+            }
+            else
+            {
+                Directory.Delete(tempUpdateDirectory);
+                Directory.CreateDirectory(tempUpdateDirectory);
+            }
+            ZipFile.ExtractToDirectory(downloadedFileFullPath, tempUpdateDirectory);
+
+            filesInDownloadedBuild = Directory.GetFiles(tempUpdateDirectory);
+            directoriesInDownloadedBuild = Directory.GetDirectories(tempUpdateDirectory);
+
+            foreach (string file in filesInAppDirectory)
+            {
+                if (File.Exists(file))
+                {
+                    File.Move(file, file + ".old");
+                }
+            }
+
+            foreach (string directory in directoriesInAppDirectory)
+            {
+                if (Directory.Exists(directory))
+                {
+                    Directory.Move(directory, directory + ".old");
+                }
+            }
+
+            foreach (string file in filesInDownloadedBuild)
+            {
+                if (File.Exists(file))
+                {
+                    File.Move(file, Path.Join(Directory.GetCurrentDirectory(), Path.GetFileName(file)));
+                }
+            }
+
+            foreach (string directory in directoriesInDownloadedBuild)
+            {
+                if (Directory.Exists(directory))
+                {
+                    string directoryName = new DirectoryInfo(directory).Name;
+                    string newDirectory = Path.Join(Directory.GetCurrentDirectory(), directoryName);
+                    Directory.Move(directory, newDirectory); 
+                }
+            }
 
         }
 
@@ -68,7 +191,6 @@ namespace BO.Elastic.Panel.ViewModels
             try
             {
                 HttpClient client = new HttpClient();
-     
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://213.32.122.228/Bo.Elastic.Panel/version");
 
                 int version;
