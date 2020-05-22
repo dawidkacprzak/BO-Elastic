ffdd70d886a9fc01a194d1b5fc1465c860f331e7
Oskar Szymański <Oskar.Szymanski@oponeo.pl>
2020-05-22 12:41:29 +0200

    MainPageWindow edit and add Update Configuration

--- BO.Elastic/BO.Elastic.Panel/MainPageWindow.xaml.cs
+++ BO.Elastic/BO.Elastic.Panel/MainPageWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BO.Elastic.Panel.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -21,6 +22,8 @@ namespace BO.Elastic.Panel
         {
             try
             {
+                MainPageWindowViewModel viewModel = new MainPageWindowViewModel();
+                this.DataContext = viewModel;
                 WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
                 InitializeComponent();
             }
--- BO.Elastic/BO.Elastic.Panel/ViewModels/MainPageWindowViewModel.cs
+++ BO.Elastic/BO.Elastic.Panel/ViewModels/MainPageWindowViewModel.cs
@@ -3,15 +3,49 @@ using System.Windows.Input;
 using System.Windows;
 using BO.Elastic.Panel.Command;
 using System;
+using System.Collections.ObjectModel;
+using BO.Elastic.BLL.Model;
+using BO.Elastic.BLL;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace BO.Elastic.Panel.ViewModels
 {
-    public class MainPageWindowViewModel
+    public class MainPageWindowViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<Service> clusters;
+
+        public ObservableCollection<Service> Clusters
+        {
+            get => clusters; set
+            {
+                if (value != null)
+                {
+                    this.clusters = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+
+        public MainPageWindowViewModel()
+        {
+
+            ConfigurationController configurationController = new ConfigurationController();
+            Clusters = new ObservableCollection<Service>(configurationController.DownloadConfiguration());
+        }
+
         public ICommand CloseAppEvent => new BasicCommand(new Action(() =>
         {
             CloseApplication();
         }));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
 
         public void CloseApplication()
         {
