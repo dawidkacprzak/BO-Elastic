c99fd44f4dcb6b3387789ff117ff88a980e18c20
tpruzynski <pruzynski.t@gmail.com>
2020-06-23 11:05:15 +0200

    LoginData

---
+++ BO.Elastic/BO.Elastic.BLL/Model/LoginData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BO.Elastic.BLL.Model
+{
+    public class LoginData
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
