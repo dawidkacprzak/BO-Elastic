579a8ab902e648d3d2cb434b6ee8108836b679bb
capthapl <dawidkacprzak@icloud.com>
2020-07-03 13:43:07 +0200

    Added tests

---
+++ BO.Elastic/BO.Elastic.BLL.Test/ElasticMappingIntegration.cs
@@ -0,0 +1,40 @@
+﻿using BO.Elastic.BLL.ElasticCore;
+using BO.Elastic.BLL.Exceptions;
+using BO.Elastic.BLL.Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BO.Elastic.BLL.Test
+{
+    public class ElasticMappingIntegration
+    {
+        [Test]
+        [TestCase("qazxswedcvc")]
+        [TestCase("Adsd")]
+        [TestCase("255324332")]
+        public void FailIndexExists(string indexName)
+        {
+            NetworkAddress address = new NetworkAddress("10.10.1.214", 9201);
+            NextWrap nw = new NextWrap(address);
+
+            Assert.False(nw.IndexExists(indexName));
+        }
+
+        [Test]
+        [TestCase("qazxswedcvc")]
+        [TestCase("Adsd")]
+        [TestCase("255324332")]
+        public void FailClusterDoesNotExistIndexExists(string indexName)
+        {
+            Assert.Throws<ClusterNotConnectedException>(() =>
+            {
+                NetworkAddress address = new NetworkAddress("10.10.1.14", 9201);
+                NextWrap nw = new NextWrap(address);
+
+                Assert.False(nw.IndexExists(indexName));
+            });
+        }
+    }
+}
